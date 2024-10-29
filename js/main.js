fb20675ea8d31cf8df1d9cf78f515331bb3cc4ee
Oleksandr D <derevianko.99@ukr.net>
2024-10-30 00:31:05 +0200

    update

--- js/main.js
+++ js/main.js
@@ -149,7 +149,6 @@ function initializeSliders() {
 }
 
 function openTab(tabId) {
-    console.log(`Opening tab: ${tabId}`); // Проверка, вызывается ли функция
 
     // Убираем активный класс со всех вкладок
     document.querySelectorAll('.tabcontent').forEach(function(tab) {
@@ -171,7 +170,6 @@ function openTab(tabId) {
     const activeButton = document.querySelector(`.tablinks[data-tab="${tabId}"]`);
     if (activeButton) {
         activeButton.classList.add('active');
-        console.log(`Active button: ${activeButton}`); // Проверка, была ли найдена кнопка
     }
 }
 
@@ -267,4 +265,101 @@ document.querySelector('.toggle-all').addEventListener('click', toggleAllJobs);
 
 // Добавляем переключение состояния
 
+var formContainer = $('#form-container');
 
+bindFormClick();
+//Opening the form
+function bindFormClick(){
+  $(formContainer).on('click', function(e) {
+    e.preventDefault();
+    toggleForm();
+    //Ensure container doesn't togleForm when open
+    $(this).off();
+  });
+}
+
+//Closing the form
+$('#form-close, .form-overlay').click(function(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  toggleForm();
+  bindFormClick();
+});
+
+function toggleForm() {
+    $(formContainer).toggleClass('expand');
+    $(formContainer).children().toggleClass('expand');
+    $('body').toggleClass('show-form-overlay');
+    $('.form-submitted').removeClass('form-submitted');
+  
+    // Показать или скрыть крестик
+    $('#form-close').toggleClass('expand');
+  }
+  
+
+//Form validation
+$('form').submit(function() {
+  var form = $(this);
+  form.find('.form-error').removeClass('form-error');
+  var formError = false;
+  
+  form.find('.input').each(function() {
+    if ($(this).val() == '') {
+      $(this).addClass('form-error');
+      $(this).select();
+      formError = true;
+      return false;
+    }
+    else if ($(this).hasClass('email') && !isValidEmail($(this).val())) {
+      $(this).addClass('form-error');
+      $(this).select();
+      formError = true;
+      return false;
+    }
+  });
+  
+  if (!formError) {
+    $('body').addClass('form-submitted');
+    $('#form-head').addClass('form-submitted'); 
+    setTimeout(function(){
+      $(form).trigger("reset");
+    }, 1000);
+  }
+  return false;
+});
+
+function isValidEmail(email) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(email);
+};
+
+// Получаем все карточки
+const cards = document.querySelectorAll('.card');
+
+// Перебираем каждую карточку и добавляем обработчики событий
+cards.forEach(card => {
+    const circle = card.querySelector('.circle1, .circle2, .circle3, .circle4, .circle5, .circle6'); // выбираем круг внутри текущей карточки
+
+    // Скрываем круг по умолчанию
+    circle.style.display = 'none';
+
+    card.addEventListener('mouseenter', () => {
+        // Показываем круг, когда курсор входит в карточку
+        circle.style.display = 'block';
+    });
+
+    card.addEventListener('mouseleave', () => {
+        // Скрываем круг, когда курсор покидает карточку
+        circle.style.display = 'none';
+    });
+
+    card.addEventListener('mousemove', (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        // Устанавливаем положение круга
+        circle.style.left = `${x - circle.offsetWidth / 2}px`;
+        circle.style.top = `${y - circle.offsetHeight / 2}px`;
+    });
+});
